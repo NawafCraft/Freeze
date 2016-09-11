fbd87d7e3a1c0e52c008114303e0790491525c44
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-10 17:04:10 -0700

    Update Nawaf1b.php

--- src/NawafPluginFrz/Nawaf1b.php
+++ src/NawafPluginFrz/Nawaf1b.php
@@ -5,7 +5,6 @@ namespace NawafPluginFrz;
 class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
     
     public $freeze = array();
-
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
@@ -20,23 +19,26 @@ class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event
             if($args[1] == "@a"){
                 foreach ($this->getServer()->getOnlinePlayers() as $p){
                     $this->getClass()->addFreeze($p->getName());
+                    $sender->sendMessage("All Players Freezing");
                 }
              }
-             
-             if($args[1] == "name"){
-             $this->getClass()->addFreeze($args[2]);
-              }
+             if($args[1] !== "@a"){
+             $this->getClass()->addFreeze($args[1]);
+             $sender->sendMessage("Now ".$args[1]." Freezing");
+             }
+              
            }
            if($args[0] == "remove"){
             if($args[1] == "@a"){
                 foreach ($this->getServer()->getOnlinePlayers() as $p){
                     $this->getClass()->removeFreeze($p->getName());
+                    $sender->sendMessage("All Players is not Freezing");
                 }
              }
-             
-             if($args[1] == "name"){
-             $this->getClass()->removeFreeze($args[2]);
-              }
+             if($args[1] !== "@a"){
+             $this->getClass()->removeFreeze($args[1]);
+             $sender->sendMessage("Now ".$args[1]." is not Freezing");
+             }
            }
         }
     }
@@ -49,5 +51,4 @@ class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event
         }
     }
 }
-
 ?>
