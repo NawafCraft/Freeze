2d9ed881fcb98b0617d5011c22e29ab81449c983
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-10 17:30:24 -0700

    Update Nawaf1b.php

--- src/NawafPluginFrz/Nawaf1b.php
+++ src/NawafPluginFrz/Nawaf1b.php
@@ -4,6 +4,8 @@ namespace NawafPluginFrz;
 
 class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
     
+    public $freeze = array();
+    
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
@@ -17,12 +19,12 @@ class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event
            if($args[0] == "add"){
             if($args[1] == "@a"){
                 foreach ($this->getServer()->getOnlinePlayers() as $p){
-                    $this->getClass()->addFreeze($p->getName());
-                    $sender->sendMessage("All Players Freezing");
+                    $this->getClass()->addFreeze($this->freeze,$p->getName());
                 }
+                    $sender->sendMessage("All Players Freezing");
              }
              if($args[1] !== "@a"){
-             $this->getClass()->addFreeze($args[1]);
+             $this->getClass()->addFreeze($this->freeze,$args[1]);
              $sender->sendMessage("Now ".$args[1]." Freezing");
              }
               
@@ -30,12 +32,12 @@ class Nawaf1b extends \pocketmine\plugin\PluginBase implements \pocketmine\event
            if($args[0] == "remove"){
             if($args[1] == "@a"){
                 foreach ($this->getServer()->getOnlinePlayers() as $p){
-                    $this->getClass()->removeFreeze($p->getName());
-                    $sender->sendMessage("All Players is not Freezing");
+                    $this->getClass()->removeFreeze($this->freeze,$p->getName());
                 }
+                $sender->sendMessage("All Players is not Freezing");
              }
              if($args[1] !== "@a"){
-             $this->getClass()->removeFreeze($args[1]);
+             $this->getClass()->removeFreeze($this->freeze,$args[1]);
              $sender->sendMessage("Now ".$args[1]." is not Freezing");
              }
            }
